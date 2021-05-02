5533b8222cb893986f5600bf3b65dd3b89cb1a2d
Jiwon Lee <jlee671@calpoly.edu>
2021-05-02 14:20:27 -0700

    yay

--- gsv_first_constraint.py
+++ gsv_first_constraint.py
@@ -3,9 +3,9 @@
 
 from z3 import *
 
-x, y, z = Ints('x y z')
+a1, a2, a3, a4, a5, a6, l, l1, l2 = Ints('a1 a2 a3 a4 a5 a6 l l1 l2')
 s = Solver()
-s.add(x + y + z < x + y + z)
+s.add((((50*a1*l1) - (a3*l1) - l1) + ((50*a4*l2) - (a6*l2) - l2) == -l) and ((a2*l1)+(a5*l2) == 0))
 print(s.sexpr())
 print("Checking...")
 print(s.check())
