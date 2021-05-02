e58b9a28bdb47298d4302baa910a0d1853bf9d4b
Parker Mitchell <parker.mitchell1@gmail.com>
2021-05-02 14:39:09 -0700

    365&7

--- gsv_first_constraint.py
+++ gsv_first_constraint.py
@@ -5,7 +5,14 @@ from z3 import *
 
 a1, a2, a3, a4, a5, a6, l, l1, l2 = Ints('a1 a2 a3 a4 a5 a6 l l1 l2')
 s = Solver()
-s.add((((50*a1*l1) - (a3*l1) - l1) + ((50*a4*l2) - (a6*l2) - l2) == -l) and ((a2*l1)+(a5*l2) == 0))
+first = (((50 * a1 * l1) - (a3 * l1) - l1) + ((50 * a4 * l2) - (a6 * l2) - l2) == -l)
+second = ((a2 * l1) + (a5 * l2) == 0)
+and_res = And(first, second)
+
+# s.add(And((((50 * a1 * l1) - (a3 * l1) - l1) + ((50 * a4 * l2) - (a6 * l2) - l2) == -l), ((a2 * l1) + (a5 * l2) == 0)))
+# s.add((((50*a1*l1) - (a3*l1) - l1) + ((50*a4*l2) - (a6*l2) - l2) == -l) and ((a2*l1)+(a5*l2) == 0))
+
+s.add(and_res)
 print(s.sexpr())
 print("Checking...")
 print(s.check())
